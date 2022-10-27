16f96ecd961f1d116409d28a6fd0310858e6c5e8
Arostegui Maranto <arosteguimaranto@outlook.com>
2022-10-27 13:00:55 -0500

    Mapbox Search API y Token de acceso

--- models/busquedas.js
+++ models/busquedas.js
@@ -14,7 +14,7 @@ class Busquedas {
             // peticion http
             //console.log('ciudad', lugar); 
 
-            const resp = await axios.get('https://reqres.in/api/users?page=2')
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/poza%20rica%20.json?proximity=ip&types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1IjoiYXJvc3RlZ3VpbWFyYW50byIsImEiOiJjbDlvbHRibjUwMnU1NDBwbXIzYml2NDNzIn0.q-ozXy3Ppd2TbRMlpWJUCA')
             console.log(resp.data);
 
             return []
